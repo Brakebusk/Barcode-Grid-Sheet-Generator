4ec1d9419a971857f7fc8f4c6bd05c47c0b8f495
Brakebusk <hermis98@hotmail.com>
2018-08-11 13:29:19 +0200

    Changed code to code39 without checksum digit

--- BarcodeHelper.py
+++ BarcodeHelper.py
@@ -2,14 +2,14 @@ import barcode
 from barcode.writer import ImageWriter
 
 class BarcodeHelper:
-    def __init__(self, content, bctype="ean8"):
+    def __init__(self, content, bctype="code39"):
         bcclass = barcode.get_barcode_class(bctype)
 
-        self.barcodeImage = bcclass(content, writer=ImageWriter())
+        self.barcodeImage = bcclass(content, writer=ImageWriter(), add_checksum=False)
     
     def getBarcodeImage(self):
         return self.barcodeImage.render({
-            'module_width': 0.35,
+            'module_width': 0.38,
             'font_size': 16,
             'text_distance': 1
         })
--- grid.py
+++ grid.py
@@ -14,20 +14,21 @@ outputDimensions = sheetResolutions['A4-Print']; #Pixel dimensions of each outpu
 
 margins = (25, 40) #right/left, up/down in pixels
 padding = (20, 25) #right/left, up/down padding withing each barcode grid space
+interSpace = (5, 0) #horizontal, vertical Space between each grid block
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #calculate pixel dimensions of barcodes on the grid:
-barcodeDimensions = (int((outputDimensions[0] - 2 * margins[0]) / gridWidth), int((outputDimensions[1] - 2 * margins[1]) / gridHeight))
+barcodeDimensions = (int((outputDimensions[0] - 2 * margins[0] - (gridWidth - 1) * interSpace[0]) / gridWidth), int((outputDimensions[1] - 2 * margins[1] - (gridHeight - 1) * interSpace[1]) / gridHeight))
 
 def createSequentialBarcodes():
     #Barcode generation config:
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     prefix = ""
-    suffix = "2018"
+    suffix = ""
     
     start = 101
     end = 299
-    padNumToLength = 0 #pad so that each number has the same number of digits by adding necessary 0-es at the start
+    padNumToLength = 3 #pad so that each number has the same number of digits by adding necessary 0-es at the start
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -59,8 +60,8 @@ def createSheet(buffer):
         imgFile = imgFile.resize((int(ratio * imgFile.size[0]), int(ratio * imgFile.size[1])))
 
         #center barcode image in box
-        offsetX = int(margins[0] + padding[0] + column * barcodeDimensions[0] + (barcodeDimensions[0] - imgFile.size[0]) / 2)
-        offsetY = int(margins[1] + padding[1] + row * barcodeDimensions[1] + (barcodeDimensions[1] - imgFile.size[1]) / 2)
+        offsetX = int(margins[0] + padding[0] + column * (barcodeDimensions[0] + interSpace[0]) + (barcodeDimensions[0] - imgFile.size[0]) / 2)
+        offsetY = int(margins[1] + padding[1] + row * (barcodeDimensions[1] + interSpace[1]) + (barcodeDimensions[1] - imgFile.size[1]) / 2)
 
         sheet.paste(imgFile, (offsetX, offsetY)) #place barcode onto the sheet at the correct location
         column += 1
